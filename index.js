ef0252f41ba038f65f776d5872509d6530bc870a
Basavaraj Dhadake <basavarajdhadake1@gmail.com>
2025-08-10 20:27:37 +0530

    Add files via upload
    Signed-off-by: Basavaraj Dhadake <basavarajdhadake1@gmail.com>

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-// let mens = document.querySelector(".nav-men");
-// mens.addEventListener("click", () => {
-//   window.location.href = "mens.html"; 
-//   console.log("he clicked mens phage")
-// });
+let mens = document.querySelector(".nav-men");
+mens.addEventListener("click", () => {
+  window.location.href = "mens.html"; 
+  console.log("he clicked mens phage")
+});
  window.addEventListener("DOMContentLoaded", () => {
   let mens = document.querySelector(".nav-men");
-  if (mens) {
-    mens.addEventListener("click", () => {
-      window.location.href = "mens.html"; 
-      console.log("he clicked mens page");
-    });
-  } else {
-    console.error("Element '.nav-men' not found.");
-  }
+  
 });
 
 let profile = document.querySelector(".nav-profile");
@@ -32,7 +25,66 @@ let wishlist= document.querySelector(".list");
 wishlist.addEventListener("click", () => {
   window.location.href = "wishlist.html"; 
 });
-let kids= document.querySelector(".nav-kid");
+let kids = document.querySelector(".nav-kid");
 kids.addEventListener("click", () => {
-  window.location.href = "kids.html"; 
+  window.location.href = "kids.html";
+});
+
+let Box = document.querySelector(".box1");
+Box.addEventListener("click", () => {
+  window.location.href = "kids.html";  
+});
+let Box2 = document.querySelector(".box2");
+Box2.addEventListener("click", () => {
+  window.location.href = "womens.html";  
+});
+let Box3 = document.querySelector(".box3");
+Box3.addEventListener("click", () => {
+  window.location.href = "mens.html";  
+});
+
+let Box4 = document.querySelector(".box4");
+Box4.addEventListener("click", () => {
+  window.location.href = "womens.html";  
+});
+let Box5 = document.querySelector(".box5");
+Box5.addEventListener("click", () => {
+  window.location.href = "mens.html";  
+});
+document.addEventListener('DOMContentLoaded', () => {
+  const backLink = document.getElementById('backLink');
+  backLink.addEventListener('click', (e) => {
+   
+    if (window.history.length > 1) {
+      e.preventDefault();        
+      window.history.back();
+    }
+
+  });
+});
+document.addEventListener('DOMContentLoaded', () => {
+  const backLinkprofile = document.getElementById('backLinkprofile');
+  backLinkprofile.addEventListener('click', (e) => {
+   
+    if (window.history.length > 1) {
+      e.preventDefault();        
+      window.history.back();
+    }
+
+  });
+});
+
+
+let btmmode=document.querySelector(".modes");
+let curentmode="light";
+btmmode.addEventListener("click",() =>{
+  if(curentmode==="light"){
+    curentmode="dark";
+    document.querySelector("body").style.backgroundColor="black";
+  }
+  else{
+    curentmode="light";
+     document.querySelector("body").style.backgroundColor="white";
+  }
+
 });
